89492275d7fadc5df57fef9fadfbc5a38a5590e3
Albert <sion2010@hotmail.com>
2017-07-17 10:20:35 -0300

    Se modifica Hello World!... a World...!

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from pyramid.response import Response
 
 def hello_world(request):
     print('Incoming request')
-    return Response("<body><h1>Hello World.!</h1></body>")
+    return Response("<body><h1>Hello World...!</h1></body>")
 
 
 if __name__ == '__main__':
